7c27c06e774072cdb486ab074725ae3bb9b73634
edu-rm <eduardomeireles3232@gmail.com>
2021-04-04 23:43:13 -0300

    participants table

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import GlobalStyle from './styles/GlobalStyle';
 
 import Participants from './pages/Participants';
-
+import "antd/dist/antd.css";
 
 function App() {
   return (
--- src/pages/Participants/index.js
+++ src/pages/Participants/index.js
@@ -1,21 +1,51 @@
 import axios from 'axios';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+
+import { Table } from 'antd';
 
 import { Container } from './styles';
 
 // import { participants } from '../../data/participants';
 
 function Participants() {
-  // console.log('participants', participants)
+  const [participantsSerialized, setParticipantsSerialized] = useState();
   useEffect(() => {
     axios.get('https://data.directory.openbankingbrasil.org.br/participants').then(res => {
-      console.log(res.data);
+      const serialized = res.data.map(item => {
+        return {
+          name: item.OrganisationName,
+          developerPortal: item.AuthorisationServers[0].DeveloperPortalUri
+        }
+      })
+      setParticipantsSerialized(serialized);
     })
   }, [])
 
+  // const participantsSerialized = participants.map(item=> {
+  //   return {
+  //     name: item.OrganisationName,
+  //     developerPortal: item.AuthorisationServers[0].DeveloperPortalUri
+  //   }
+  // })
+
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Developer portal',
+      dataIndex: 'developerPortal',
+      key: 'developerPortal',
+      render: text => <a href={text} rel="noreferrer" target="_blank">Developer portal</a>,
+    },
+  ];
+  
   return (
     <Container>
       <h1>Participants</h1>
+      <Table columns={columns} dataSource={participantsSerialized}/>
     </Container>
   );
 }
